7d735e2b2a14e17f1a39b3eb1ff34c95efceed82
bartsvoboda <bartlomiejswoboda@gmail.com>
2020-11-15 18:27:18 +0100

    add binds to exercise component

--- src/components/add-exercises.component.js
+++ src/components/add-exercises.component.js
@@ -1,6 +1,68 @@
 import React , { Component } from 'react';
 
 export default class AddExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeKcalperhour = this.onChangeKcalperhour.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration:0,
+            kcalperhour:0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e){
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    onChangeKcalperhour(e){
+        this.setState({
+            kcalperhour: e.target.value
+        });
+    }
+
+    onChangeDate(date){
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            kcalperhour: this.state.kcalperhour,
+            date: this.state.date
+        }
+    }
+
     render() {
         return (
             <h1> Dodaj cwiczenie</h1>
