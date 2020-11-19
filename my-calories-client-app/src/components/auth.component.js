600b6d2590b1ab7bc545770157af4188b2f89570
bartsvoboda <bartlomiejswoboda@gmail.com>
2020-11-20 00:49:08 +0100

    add user without auth

--- my-calories-client-app/src/components/auth.component.js
+++ my-calories-client-app/src/components/auth.component.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export default class AuthenitcatedComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: undefined
+        }
+    }
+
+    
     render() {
         return(
             <div>
--- my-calories-client-app/src/components/login.component.js
+++ my-calories-client-app/src/components/login.component.js
@@ -1,51 +1,74 @@
 import React , { Component } from 'react';
 
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+export default class AddFood extends Component {
+  constructor(props) {
+      super(props);
 
-export default class Login extends Component {
-    render() {
-        return (
-            <Form>
-              <Jumbotron className="mr-auto">
-              <h1>Witaj w aplikacji <strong> MyCalories </strong></h1>
-              <p>Aplikacja pozwala na monitorowanie kalori z jedzenia i ćwiczeń</p>
-              <p> Zaloguj się poniżej, jeżeli nie masz konta możesz założyc je klikając w poniższy przycisk</p>
-              <p>
-                 <Button variant="primary">Zerejstruj</Button>
-              </p>
+      this.onChangeEmail = this.onChangeEmail.bind(this);
+      this.onChangePassword = this.onChangePassword.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+    
+      this.state = {
+          email: '',
+          password:''
+      }
+  }
 
-              </Jumbotron>
+  onChangeEmail(e){
+      this.setState({
+          email: e.target.value
+      });
+  }
 
-              <Container> 
-                <Row className="justify-content-md-center">
-                  <Col className="mr-auto">
-                    <Form.Group controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control type="email" placeholder="Enter email" />
-                      <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                      </Form.Text>
-                    </Form.Group>
+  onChangePassword(e){
+      this.setState({
+          password: e.target.value
+      });
+  }
 
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                      <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" href="/menu">
-                      Zaloguj
-                    </Button>
-                  </Col>  
-                </Row>
-              </Container>
-            </Form>
-        )
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
     }
+    
+    console.log(user);
+    
+  }
+
+
+  render() {
+      return (
+          <div>
+            <h3>Zaloguj się</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Email: </label>
+                <input type ="email"
+                    required
+                    className="form-control"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                    />
+              </div>
+              <div className="form-group"> 
+                <label>Hasło: </label>
+                <input type="password"
+                    required
+                    className="form-control"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    />
+              </div>
+            
+              <div className="form-group">
+                <input type="submit" value="Zaloguj" className="btn btn-primary" />
+              </div>
+            </form>
+          </div>
+          )
+  }
 }
