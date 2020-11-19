de4b4437904e60c7a0391f2402319f499887b679
bartsvoboda <bartlomiejswoboda@gmail.com>
2020-11-19 23:55:02 +0100

    delete irrelevant files

---
+++ my-calories-client-app/src/components/auth.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AuthenitcatedComponent extends Component {
+    render() {
+        return(
+            <div>
+                siemka
+            </div>
+        )
+    }
+}
